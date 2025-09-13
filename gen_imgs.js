b8fbb157bed1adedcae766583bf29ae5395c743f
Coleman DuPlessie <coleyduplessie@gmail.com>
2025-09-13 13:20:28 -0400

    Huggingface API is functional!

--- gen_imgs.js
+++ gen_imgs.js
@@ -1,17 +1,23 @@
 import { KEY } from "./API_KEYS.js";
-async function genImg() {
+export async function genImg() {
+    console.log("Generating image...");
     const HFInference = (
         await import("https://cdn.skypack.dev/@huggingface/inference@2.0.0")
     ).HfInference;
     const hf = new HFInference(KEY);
+    console.log("Sending Huggingface query...");
     const blob = await hf.textToImage({
     inputs:
         "a puppy wearing a birthday hat sitting in a jungle. The jungle is dark but comfortable, the puppy is smiling. Digital art style. <lora:iu_V35:0.5> <lora:epiNoiseoffset_v2:0.5>",
     parameters: {
         negative_prompt: "easynegative",
     },
-    model: "prompthero/openjourney-v4",
+    model: "stabilityai/stabilityai/stable-diffusion-3.5-medium-tensorrt",
     });
-    const tab = window.open((target = "_blank"));
-    tab.location.href = window.URL.createObjectURL(blob);
+    console.log("Image generated!");
+    console.log(String(blob));
+    const imageUrl = URL.createObjectURL(blob);
+    window.open(imageUrl, "_blank");
+    document.querySelector("#outputImg").src = imageUrl;
+    console.log("Image opened in new tab!");
 }
