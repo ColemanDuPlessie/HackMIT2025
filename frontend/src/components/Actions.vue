3bd9c5c3ce75b0a2eea84783ec2536461594cea3
Outer Cloud <outercloudstudio@gmail.com>
2025-09-13 22:27:47 -0400

    Go Home

--- frontend/src/components/Actions.vue
+++ frontend/src/components/Actions.vue
@@ -1,5 +1,55 @@
 <script lang="ts" setup>
-import { deleteSelectedNodes } from '../lib/State'
+import { onMounted } from 'vue'
+import { deleteSelectedNodes, nodes, viewOffsetX, viewOffsetY } from '../lib/State'
+
+let xOffset = 0
+let yOffset = 0
+
+function goHome() {
+    let sumX = 0
+    let sumY = 0
+
+    for (const node of nodes.value) {
+        sumX += node.x
+        sumY += node.y
+    }
+
+    const averageX = sumX / nodes.value.length
+    const averageY = sumY / nodes.value.length
+
+    xOffset = averageX - (-viewOffsetX.value + document.body.offsetWidth / 2)
+    yOffset = averageY - (-viewOffsetY.value + document.body.offsetHeight / 2)
+
+    console.log(viewOffsetX.value, viewOffsetY.value)
+    console.log(averageX, averageY)
+    console.log(xOffset, yOffset)
+
+    // viewOffsetX.value -= xOffset
+    // viewOffsetY.value -= yOffset
+}
+
+let lastTime = Date.now()
+
+function update() {
+    const now = Date.now()
+    const deltaTime = ((now - lastTime) / 1000) * 4
+    lastTime = now
+
+    const movementX = xOffset * deltaTime
+    const movementY = yOffset * deltaTime
+
+    viewOffsetX.value -= movementX
+    viewOffsetY.value -= movementY
+
+    xOffset -= movementX
+    yOffset -= movementY
+
+    requestAnimationFrame(update)
+}
+
+onMounted(() => {
+    update()
+})
 </script>
 
 <template>
@@ -19,6 +69,7 @@ import { deleteSelectedNodes } from '../lib/State'
 
         <button
             class="flex cursor-pointer items-center px-1.5 m-1 grow select-none hover:bg-[var(--color-background)] border border-[var(--color-border)] rounded-lg transition-colors duration-100 ease-in-out hover:duration-0"
+            @click="goHome"
         >
             <span class="material-symbols-outlined text-[var(--color-text-alt)] block"> home </span>
         </button>
